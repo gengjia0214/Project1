978372027eab6f272be81f6136bc0e1e8ca497b3
Jia Geng <gjia0214@gmail.com>
2020-01-28 18:12:56 -0500

    init framework

---
+++ src/soduku.py
@@ -0,0 +1,41 @@
+
+
+class Soduku:
+    """
+    A Soduku Solver using different search algorithms
+    """
+
+    def __init__(self):
+        self.suduko = None
+        self.solution = None
+        self.memo = None
+
+    def read_soduku(self):
+        """
+        Read a soduku
+        :return:
+        """
+        pass
+
+    def solve_soduku(self, mode):
+        pass
+
+    def dfs(self):
+        pass
+
+    def bfs(self):
+        pass
+
+    def deepening(self):
+        pass
+
+    def evaluate(self):
+        pass
+
+    def compare(self):
+        pass
+
+    @staticmethod
+    def parser():
+        pass
+
