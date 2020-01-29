1741815f6e0d69ea80cc306903fde4fc6f324086
Jia Geng <gjia0214@gmail.com>
2020-01-28 20:13:29 -0500

    update docstring

--- src/sudoku.py
+++ src/sudoku.py
@@ -1,21 +1,37 @@
 import time
 import csv
 
+"""
+- A Soduku Solver using different search algorithms
+- Soduku are generated via https://qqwing.com/generate.html
+- Invalid Soduku are fetched from http://sudopedia.enjoysudoku.com/Invalid_Test_Cases.html
+- Sudokus with multiple solutions are not under considered
+- Invalid sudoku includes unsolvable sudoku and sudoku with invalid init state
+"""
+
 
 class Sudoku:
     """
-    A Soduku Solver using different search algorithms
-    Soduku are generated via https://qqwing.com/generate.html
-    Invalid Soduku are fetched from http://sudopedia.enjoysudoku.com/Invalid_Test_Cases.html
-    Sudokus with multiple solutions are not under considered
-    Invalid sudoku includes unsolvable sudoku and sudoku with invalid init state
+    To use:
+    1. Create a solver instance:        solver = Sudoku()
+    2. Load csv data:                   parsed_data = Sudoku.parser('/path/to/dir/easy.csv')
+    3. Read one data:                   sudoku_data = parsed_data[i]
+    4. Solve it with selected mode:     can_solve, time = solver.solve(mode='dfs')
+    5. Report & Read & Solve next one...
     """
 
     def __init__(self):
+        """
+        Constructor
+        Metadata Keys include:
+        'puzzle', 'solution', 'givens', 'singles', 'hidden_singles'
+        'naked_pairs', 'hidden_paris', 'triples', 'intersections', 'guesses'
+        'backtracks', 'difficulty'
+        """
 
         # sudoku data
-        self.sudoku_board = None
-        self.meta_data = None
+        self.sudoku_board = None  # sudoku board in 2-D list
+        self.meta_data = None  # meta_data
 
         # data struct for the solvers
         self.memo = None
@@ -60,7 +76,7 @@ class Sudoku:
             board = [list(string[i*9:i*9+9]) for i in range(9)]
             return board
 
-        # read data
+        # read metadata
         self.sudoku_board = to_board(sudoku_dict['puzzle'])
         self.meta_data = sudoku_dict
 
@@ -69,7 +85,7 @@ class Sudoku:
         Try to solve the soduku with selected approach
         :param mode: selected approach -> {'dfs', 'bfs', 'deepening'}
         :param repeat: solve the sudoku n times to get the average solving time
-        :return:
+        :return: bool, float -> whether the input is solvable, time spent
         """
 
         def to_str(board: list) -> str:
@@ -79,6 +95,7 @@ class Sudoku:
         k = repeat
         can_solve = False
 
+        # main loop
         start = time.time()
         while k > 0:
             if mode == 'dfs':
@@ -92,6 +109,7 @@ class Sudoku:
             k -= 1
         end = time.time()
 
+        # check correctness
         if self.meta_data['solution'] == 'False' and can_solve:
             raise Exception("Solution is incorrect!")
         elif to_str(self.sudoku_board) != self.meta_data['solution']:
@@ -105,8 +123,6 @@ class Sudoku:
     def parser(src_p) -> list:
         """
         Data parser that read the soduku metadata.
-        csv columns:
-        Puzzle,Solution,Givens,Singles,Hidden Singles,Naked Pairs,Hidden Pairs,Pointing Pairs/Triples,Box/Line Intersections,Guesses,Backtracks,Difficulty
         :param src_p: input sudoku samples in csv format
         :return: List -> parsed data that is compatible with this solver
         """
@@ -121,10 +137,11 @@ class Sudoku:
                     continue
 
                 # build metadata
-                soduku_metadata = {'puzzle': line[0], 'solution': line[1], 'givens': line[2], 'singles': line[3],
-                                   'hidden_singles': line[4], 'naked_pairs': line[5], 'hidden_paris': line[6],
-                                   'triples': line[7], 'intersections': line[8], 'guesses': line[9],
-                                   'backtracks': line[10], 'difficulty': line[11]}
+                soduku_metadata = {'puzzle': line[0], 'solution': line[1], 'givens': int(line[2]),
+                                   'singles': int(line[3]), 'hidden_singles': int(line[4]),
+                                   'naked_pairs': int(line[5]), 'hidden_paris': int(line[6]),
+                                   'triples': int(line[7]), 'intersections': int(line[8]), 'guesses': int(line[9]),
+                                   'backtracks': int(line[10]), 'difficulty': line[11]}
                 parsed_data.append(soduku_metadata)
 
         return parsed_data
